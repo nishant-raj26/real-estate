0df69e664f5fe699cfc9a2b4d70e77f785b874df
Nishant Raj <nishantraj87726@gmail.com>
2023-05-23 23:36:40 +0530

    real-estate

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Companies.css'
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+        <div className="paddings innerwidth flexCenter c-container">
+            <img src="../../../public/prologis.png" alt="" />
+            <img src="../../../public/tower.png" alt="" />
+            <img src="../../../public/equinix.png" alt="" />
+            <img src="../../../public/realty.png" alt="" />
+        </div>
+    </section>
+  )
+}
+
+export default Companies
